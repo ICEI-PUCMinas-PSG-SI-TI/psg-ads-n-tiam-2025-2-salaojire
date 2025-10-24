3d0e9050e2bcec78e6e29081d6d6ea56cca83b0d
IsaqueCN <ggomesnascimento123@gmail.com>
2025-10-24 19:55:00 -0300

    Adicionando conexão ao Firebase

--- src/packages/firebase/index.js
+++ src/packages/firebase/index.js
@@ -1,15 +1,18 @@
-import * as auth from './auth';
-import * as storage from './storage';
-import * as administradores from './firestore/administradores';
-import * as clientes from './firestore/clientes';
-import * as itens from './firestore/itens';
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 
-export default {
-  auth,
-  storage,
-  firestore: {
-    administradores,
-    clientes,
-    itens,
-  },
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID,
 };
+
+const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
