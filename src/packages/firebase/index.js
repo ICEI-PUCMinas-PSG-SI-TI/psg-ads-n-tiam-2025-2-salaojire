53b9c16631917be0587a22d2f0aa6c72ab09f583
IsaqueCN <ggomesnascimento123@gmail.com>
2025-10-24 21:41:33 -0300

    Estruturando API para firebase

--- src/packages/firebase/index.js
+++ src/packages/firebase/index.js
@@ -1,18 +1,9 @@
-import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
-import { getStorage } from 'firebase/storage';
+import * as clientes from './clientes.js';
+import * as agendamentos from './agendamentos.js';
+import * as auth from './auth.js';
 
-const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_APP_ID,
+export default {
+  clientes,
+  agendamentos,
+  auth
 };
-
-const app = initializeApp(firebaseConfig);
-export const firestore = getFirestore(app);
-export const auth = getAuth(app);
-export const storage = getStorage(app);
