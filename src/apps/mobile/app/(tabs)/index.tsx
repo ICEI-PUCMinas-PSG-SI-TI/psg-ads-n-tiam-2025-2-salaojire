146904c5b36637af3818ca1cc56740493e04a2d8
IsaqueCN <ggomesnascimento123@gmail.com>
2025-10-26 23:48:05 -0300

    Realizando ultimas mudanças para entrega

--- src/apps/mobile/app/(tabs)/index.tsx
+++ src/apps/mobile/app/(tabs)/index.tsx
@@ -1,10 +1,16 @@
+import { Button } from "@react-navigation/elements";
 import { View, Text, StyleSheet } from "react-native";
+import FirebaseAPI from "@packages/firebase";
+import { useAuth } from "../context/AuthContext";
 
-export default function Homepage() {
+export default function HomepageScreen() {
+  const { user, initializing, logout } = useAuth();
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Homepage</Text>
-      <Text style={styles.text}>Aqui você poderá visualizar a homepage do app.</Text>
+      <Text style={styles.text}>Aqui ficará a homepage do aplicativo.</Text>
+      <Text>Logado atualmente como: {user.email}</Text>
+      <Button onPress={logout}>Logout</Button>
     </View>
   );
 }
