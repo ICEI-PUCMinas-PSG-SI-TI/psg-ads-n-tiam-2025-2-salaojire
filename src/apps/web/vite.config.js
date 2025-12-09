19a69d7ae7fdd2b16fe359dd2a16fe013939af40
IsaqueCN <ggomesnascimento123@gmail.com>
2025-12-09 20:00:01 -0300

    Resolvendo reconhecimento de dotenv

--- src/apps/web/vite.config.js
+++ src/apps/web/vite.config.js
@@ -1,26 +1,38 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    // Isso é crucial para Monorepos: Evita que o React seja carregado duas vezes
-    dedupe: ['react', 'react-dom'],
-    alias: {
-      // Seu alias para os pacotes compartilhados
-      '@packages': path.resolve(__dirname, '../../packages'), 
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    envPrefix: ['VITE_', 'EXPO_PUBLIC_'], 
+    resolve: {
+      dedupe: ['react', 'react-dom'],
+      alias: {
+        '@packages': path.resolve(__dirname, '../../packages'), 
+      },
     },
-  },
-  server: {
-    fs: {
-      // Permite importar arquivos fora da pasta do projeto (caso precise do @packages)
-      allow: ['..'],
+
+    define: {
+      'process.env.EXPO_PUBLIC_API_KEY': JSON.stringify(env.EXPO_PUBLIC_API_KEY),
+      'process.env.EXPO_PUBLIC_AUTH_DOMAIN': JSON.stringify(env.EXPO_PUBLIC_AUTH_DOMAIN),
+      'process.env.EXPO_PUBLIC_PROJECT_ID': JSON.stringify(env.EXPO_PUBLIC_PROJECT_ID),
+      'process.env.EXPO_PUBLIC_STORAGE_BUCKET': JSON.stringify(env.EXPO_PUBLIC_STORAGE_BUCKET),
+      'process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID': JSON.stringify(env.EXPO_PUBLIC_MESSAGING_SENDER_ID),
+      'process.env.EXPO_PUBLIC_APP_ID': JSON.stringify(env.EXPO_PUBLIC_APP_ID),
+
+      'process.env': {},
     },
-  },
+    server: {
+      fs: {
+        allow: ['..'],
+      },
+    },
+  }
 })
