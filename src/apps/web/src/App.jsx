e93bd34b2d930e8be2a028dff8e66a3653cb01a1
Gabriel Assis <gabriel-assis19y@hotmail.com>
2025-12-09 21:09:21 -0300

    alterando import

--- src/apps/web/src/App.jsx
+++ src/apps/web/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router';
-import { AuthProvider } from './contexts/AuthContext'; // Importe o Provider
+import { AuthProvider } from './contexts/AuthContext'; 
 import Layout from './components/Layout';
 import Homepage from './pages/Homepage';
-import CatalogoItens from './pages/CatalogoItens';
+import Catalogo from './components/homepage/Catalogo';
 import Login from './pages/Login';
 
 // Roteador geral e inicio do site, tem o Router que gerência a troca de páginas
@@ -15,7 +15,7 @@ export default function App() {
           {/* Páginas ficam dentro do Layout para que ele seja aplicado em todas) */}
           <Route path="/" element={<Layout />}>
             <Route index element={<Homepage />} />
-            <Route path="/itens" element={<CatalogoItens />} />
+            <Route path="/itens" element={<Catalogo />} />
             <Route path="/login" element={<Login />} />
           </Route>
         </Routes>
